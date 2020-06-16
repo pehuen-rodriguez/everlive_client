b8960d88cf0f5119fef5f194a51564fefccb6cda
Pehuén <pehuen.rodriguez@nayra.coop>
2020-06-15 22:14:45 -0300

    Sockets impl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Socket, Presence } from "phoenix";
+import { useLocation } from "react-router-dom";
+import random_name from "node-random-name";
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
+const socket = new Socket("ws://localhost:4000/socket", {
+  params: { username: random_name() },
+});
+socket.connect();
+let channel;
+let presence;
 
 function App() {
+  const [userList, setUsersList] = useState(new Set());
+  const [coordsList, setCoordsList] = useState(new Map());
+  const [presences, setPresences] = useState();
+
+  const query = useQuery();
+  const id = query.get("id");
+
+  useEffect(() => {
+    channel = socket.channel(`room:${id}`);
+    channel
+      .join()
+      .receive("ok", ({ userId }) => console.log(`Received userId: ${userId}`))
+      .receive("error", ({ reason }) => console.log("failed join", reason))
+      .receive("timeout", () =>
+        console.log("Networking issue. Still waiting...")
+      );
+
+    presence = new Presence(channel);
+
+    channel.on("move", (msg) => {
+      const { user, x, y, z } = msg;
+      setUsersList((prev) => new Set(prev.add(user)));
+      setCoordsList((prev) => {
+        return new Map(prev.set(user, { x, y, z }));
+      });
+    });
+    presence.onSync(() => {
+      setPresences((prev) => {
+        return presence.list();
+      });
+    });
+    return () => {
+      channel.off("move");
+    };
+  }, [id]);
+
+  const handleMouseMove = (e) => {
+    if (channel) {
+      channel.push(
+        "move",
+        {
+          posX: e.nativeEvent.clientX,
+          posY: 0,
+          posZ: e.nativeEvent.clientY,
+        },
+        10000
+      );
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="App" onMouseMove={handleMouseMove}>
+      <p>{JSON.stringify(presences)}</p>
+      {[...userList.values()].map((user) => (
+        <p key={user}>{`${user}:: ${JSON.stringify(coordsList.get(user))}`}</p>
+      ))}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
